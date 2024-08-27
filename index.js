ac0ca028fd567fe51b53761e7d81e5cba15e9e8a
Kevin <kevin.chuca@live.com.ar>
2024-08-27 12:19:26 -0300

    estructura

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express' 
+import path from 'path'  // Importa el módulo 'path' para manejar y transformar rutas de archivos.
+import { fileURLToPath } from 'url'  // Importa la función 'fileURLToPath' del módulo 'url' para convertir una URL de archivo en una ruta de archivo
+const __filename = fileURLToPath (import.meta.url)
+const __dirname = path.dirname (__filename)
 
 const app = express()
 
@@ -6,4 +10,14 @@ const port = 5001
 
 app.listen(port, () =>{
     console.log(`Servidor levantado en el puerto ${port}`)
+});
+
+app.use(express.static('./Public')) 
+
+app.get('/Home', (req, res)=>{
+    res.sendFile(path.join(__dirname,'Public','Pages','Home','index.html'));
+});
+
+app.get('/QuienesSomos', (req, res)=>{
+    res.sendFile(path.join(__dirname,'Public','Pages','QuienesSomos','index.html'));
 });
