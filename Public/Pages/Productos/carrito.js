b70d84806955b3ddb9b42f2eb5810d8607149eb6
Kevin <kevin.chuca@live.com.ar>
2024-09-24 17:25:01 -0300

    actualizacion

--- Public/Pages/Productos/carrito.js
+++ Public/Pages/Productos/carrito.js
@@ -140,3 +140,13 @@ function comprarCarrito() {
     contenedorCarritoComprado.classList.remove("disabled");
 
 }
+
+const loginBtn = document.getElementById("loginBtn")
+
+if (loginBtn) {
+  if (window.localStorage.getItem("user")) {
+    loginBtn.innerHTML = `<a class="btn btn-primary" onclick='window.localStorage.removeItem("user"); window.location.reload();'>Cerrar sesion</a>`
+  } else {
+    loginBtn.innerHTML = `<a href="/login" class="btn btn-primary">Iniciar sesion</a>`
+  }
+}
